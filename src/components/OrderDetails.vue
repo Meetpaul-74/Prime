4f12f6464f93e3e60f1e24a61dc0a87cb5b8c160
sahil <sahil@xyrintech.com>
2022-11-18 00:22:00 +0530

    Order Details design fixed.

--- src/components/OrderDetails.vue
+++ src/components/OrderDetails.vue
@@ -69,7 +69,7 @@
     </div>
     <div class="col-4">
       <div class="card p-fluid">
-        <h5>Rider Details</h5>
+        <h5>Company Details</h5>
         <div class="flex align-items-center gap-3">
           <img class="w-2" src="\images\img\person.png" alt="" />
           <span class="font-semibold">Sahil Suthar</span>
@@ -103,18 +103,57 @@
             <td>$9.99</td>
           </tr>
           <tr
-            class="flex justify-content-between border-bottom-1 border-gray-300 py-2 bg-blue-50"
+            class="flex justify-content-between border-bottom-1 border-gray-300 py-2"
           >
+            <th class="font-semibold">Tax Total:</th>
+            <td>$9.99</td>
+          </tr>
+        </table>
+      </div>
+      <!-- <div class="card">
+        <table class="w-full">
+          <tr class="flex justify-content-between">
             <th>Profit:</th>
             <td class="font-bold text-gray-900">$19.99</td>
           </tr>
         </table>
+      </div> -->
+    </div>
+    <div class="col-12 grid">
+      <div class="col-3">
+        <div class="card p-fluid">
+          <div class="flex flex-column align-items-center gap-3">
+            <img class="w-2" src="\images\img\person.png" alt="" />
+            <div class="font-bold text-lg">Name</div>
+            <span class="font-semibold">Sahil Suthar</span>
+          </div>
+        </div>
       </div>
-      <div class="card p-fluid">
-        <h5>Delivery Address</h5>
-        <div class="flex align-items-center gap-3">
-          <img class="w-2" src="\images\img\person.png" alt="" />
-          <span class="font-semibold">Sahil Suthar</span>
+      <div class="col-3">
+        <div class="card p-fluid">
+          <div class="flex flex-column align-items-center gap-3">
+            <img class="w-2" src="\images\img\delivery-address.png" alt="" />
+            <div class="font-bold text-lg">Addres</div>
+            <span class="font-semibold">Sahil Suthar</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="card p-fluid">
+          <div class="flex flex-column align-items-center gap-3">
+            <img class="w-2" src="\images\img\telephone.png" alt="" />
+            <div class="font-bold text-lg">Phone Number</div>
+            <span class="font-semibold">+91-98888-88889</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="card p-fluid">
+          <div class="flex flex-column align-items-center gap-3">
+            <img class="w-2" src="\images\img\email.png" alt="" />
+            <div class="font-bold text-lg">Email</div>
+            <span class="font-semibold">Sahil@suthar.com</span>
+          </div>
         </div>
       </div>
     </div>
